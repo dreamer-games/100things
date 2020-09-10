d2ce32cd4d784c960cee45815ffd38b8be09cf7c
zarazhu <1043076284@qq.com>
2020-09-10 22:24:44 +0800

    v1.0

---
+++ app.js
@@ -0,0 +1,12 @@
+App({globalData:{
+  // select_list:[],
+  // userInfo:{}
+  select_list:[],
+  userInfo:{avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLoUSXJSW5hf5ExibTqN3icthOzKCm7nDA3EfGZ5eiaHm7Siaj5moFNYHSX7ic6z4KL6wichGD5PrZ6bQHA/132",
+  city: "Jinhua",
+  country: "China",
+  gender: 1,
+  language: "zh_CN",
+  nickName: "仑哥",
+  province: "Zhejiang"}
+}})
